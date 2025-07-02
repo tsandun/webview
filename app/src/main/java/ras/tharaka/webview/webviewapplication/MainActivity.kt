4369e72f0fc52c216c7d8c2ff9b533e28c5f07b1
Sandun <sandun@team.bhasha.lk>
2025-07-03 00:05:40 +0530

    initial commit

---
+++ app/src/main/java/ras/tharaka/webview/webviewapplication/MainActivity.kt
@@ -0,0 +1,80 @@
+package ras.tharaka.webview.webviewapplication
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.util.Log
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        val urlInput = findViewById<EditText>(R.id.urlInput)
+        val loadButton = findViewById<Button>(R.id.loadButton)
+        val webView = findViewById<WebView>(R.id.webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.cacheMode = WebSettings.LOAD_NO_CACHE
+        webView.webViewClient = object : WebViewClient(){
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                Log.d("WebView", "Page loading started: $url")
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                Log.d("WebView", "Page loading finished: $url")
+            }
+
+            override fun onReceivedError(
+                view: WebView,
+                request: WebResourceRequest,
+                error: WebResourceError
+            ) {
+                super.onReceivedError(view, request, error)
+                Log.e("WebView", "Error loading ${request.url}: ${error.description}")
+                Toast.makeText(view.context, "Error loading page", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onReceivedHttpError(
+                view: WebView,
+                request: WebResourceRequest,
+                errorResponse: WebResourceResponse
+            ) {
+                super.onReceivedHttpError(view, request, errorResponse)
+                Log.e("WebView", "HTTP error ${errorResponse.statusCode} for ${request.url}")
+            }
+        }
+
+        loadButton.setOnClickListener {
+            var url = urlInput.text.toString().trim()
+
+            if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                url = "https://$url"
+            }
+
+            webView.loadUrl(url)
+        }
+    }
+}
